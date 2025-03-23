d9d20ec3d35b243f4d5a0bcb027aa8cbaac6f335
bekir <yazganebubekir4@gmail.com>
2025-03-22 19:14:31 -0700

    translate errors fixed

--- src/App.tsx
+++ src/App.tsx
@@ -42,26 +42,26 @@ function App() {
     <div className="min-h-screen flex flex-col justify-center items-center bg-black text-white p-8 fixed top-0 left-0 w-screen h-screen overflow-hidden">
       <div className="grid grid-cols-3 items-center gap-2">
           <div className="bg-white/10 backdrop-blur-lg rounded-xl p-8 min-w-[200px] transition-transform duration-300 col-span-3 flex items-center justify-center flex-col gap-2">
-            <span className="text-5xl md:text-6xl font-bold block text-white ">
+            <span className="text-5xl md:text-6xl font-bold block text-white " translate='no'>
               {timeLeft.days}
             </span>
-            <span className="text-xl text-gray-300 uppercase tracking-wider mt-2 block">
+            <span className="text-xl text-gray-300 uppercase tracking-wider mt-2 block" translate='no'>
               Gün
             </span>
           </div>
           <div className="bg-white/10 backdrop-blur-lg rounded-xl p-8 transition-transform duration-300 flex items-center justify-center flex-col gap-2">
-            <span className="text-4xl md:text-5xl font-bold block text-white">
+            <span className="text-4xl md:text-5xl font-bold block text-white" translate='no'>
               {timeLeft.hours}
             </span>
-            <span className="text-lg text-gray-300 uppercase tracking-wider mt-2 block">
+            <span className="text-lg text-gray-300 uppercase tracking-wider mt-2 block" translate='no'>
               Saat
             </span>
           </div>
           <div className="bg-white/10 backdrop-blur-lg rounded-xl p-8 transition-transform duration-300 flex items-center justify-center flex-col gap-2">
-            <span className="text-4xl md:text-5xl font-bold block text-white">
+            <span className="text-4xl md:text-5xl font-bold block text-white" translate='no'>
               {timeLeft.minutes}
             </span>
-            <span className="text-lg text-gray-300 uppercase tracking-wider mt-2 block">
+            <span className="text-lg text-gray-300 uppercase tracking-wider mt-2 block" translate='no'>
               Dakika
             </span>
           </div>
@@ -69,12 +69,12 @@ function App() {
             <span className="text-4xl md:text-5xl font-bold block text-white">
               {timeLeft.seconds}
             </span>
-            <span className="text-lg text-gray-300 uppercase tracking-wider mt-2 block">
+            <span className="text-lg text-gray-300 uppercase tracking-wider mt-2 block" translate='no'>
               Saniye
             </span>
           </div>
 
-        <div className="fixed bottom-5 left-1/2 -translate-x-1/2 text-gray-300">seni seviyorum</div>
+        <div className="fixed bottom-5 left-1/2 -translate-x-1/2 text-gray-300" translate='no'>seni seviyorum</div>
       </div>
     </div>
   )
